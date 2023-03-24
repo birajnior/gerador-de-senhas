e0414193c3fb3dce24f82ca68022544639bc60fd
birajnior <ubiratanbernardo@gmail.com>
2023-03-24 20:21:31 -0300

    Update Códigos.
    Finalizando a estilização e responsividade e adicionando mais algumas funções.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,10 +1,29 @@
-let passwordLength = 16
 const inputEl = document.querySelector("#password")
+const upperCaseCheckEl = document.querySelector("#uppercase-check")
+const numberCheckEl = document.querySelector("#number-check")
+const symbolCheckEl = document.querySelector("#symbol-check")
+const securityIndicatorBarEl = document.querySelector("#security-indicator-bar")
+
+let passwordLength = 16
 
 function generatePassword() {
-    const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
-
+    let chars = "abcdefghjkmnpqrstuvwxyz"
+    const upperCaseChars = "ABCDEFGHJKLMNPQRSTUVWXYZ"
+    const numberChars = "123456789"
+    const symbolChars = "?!@&*()[]"
     let password = ""
+
+    if (upperCaseCheckEl.checked) {
+        chars += upperCaseChars
+    }
+
+    if (numberCheckEl.checked) {
+        chars += numberChars
+    }
+
+    if (symbolCheckEl.checked) {
+        chars += symbolChars
+    }
 
     for (let i = 0; i < passwordLength; i++) {
         const randomNumber = Math.floor(Math.random() * chars.length)
@@ -12,6 +31,59 @@ function generatePassword() {
     }
 
     inputEl.value = password
+    calculateQuality()
+    calculateFontSize()
+}
+
+function calculateQuality() {
+    const percent = Math.round(
+        (passwordLength / 64) * 35 +
+        (upperCaseCheckEl.checked ? 15 : 0) +
+        (numberCheckEl.checked ? 20 : 0) +
+        (symbolCheckEl.checked ? 30 : 0)
+    )
+
+    securityIndicatorBarEl.style.width = `${percent}%`
+
+    if (percent >= 69) {
+        securityIndicatorBarEl.classList.remove("critical")
+        securityIndicatorBarEl.classList.remove("warning")
+        securityIndicatorBarEl.classList.add("safe")
+    } else if (percent >= 50) {
+        securityIndicatorBarEl.classList.remove("safe")
+        securityIndicatorBarEl.classList.remove("critical")
+        securityIndicatorBarEl.classList.add("warning")
+    } else {
+        securityIndicatorBarEl.classList.remove("safe")
+        securityIndicatorBarEl.classList.remove("warning")
+        securityIndicatorBarEl.classList.add("critical")
+    }
+
+    if (percent >= 100) {
+        securityIndicatorBarEl.classList.add("completed")
+    } else {
+        securityIndicatorBarEl.classList.remove("completed")
+    }
+}
+
+function calculateFontSize() {
+    if (passwordLength > 45) {
+        inputEl.classList.remove("font-sm")
+        inputEl.classList.remove("font-xs")
+        inputEl.classList.add("font-xxs")
+    } else if (passwordLength > 32) {
+        inputEl.classList.remove("font-sm")
+        inputEl.classList.remove("font-xxs")
+        inputEl.classList.add("font-xs")
+    } else if (passwordLength > 22) {
+        inputEl.classList.remove("font-xxs")
+        inputEl.classList.remove("font-xs")
+        inputEl.classList.add("font-sm")
+    } else {
+        inputEl.classList.remove("font-xxs")
+        inputEl.classList.remove("font-xs")
+        inputEl.classList.remove("font-sm")
+    }
 }
 
 function copy() {
@@ -21,10 +93,16 @@ function copy() {
 const passwordLengthEl = document.querySelector("#password-length")
 passwordLengthEl.addEventListener("input", function () {
     passwordLength = passwordLengthEl.value
+    document.querySelector("#password-length-text").innerText = passwordLength
     generatePassword()
 })
 
+upperCaseCheckEl.addEventListener("click", generatePassword)
+numberCheckEl.addEventListener("click", generatePassword)
+symbolCheckEl.addEventListener("click", generatePassword)
+
 document.querySelector("#copy-1").addEventListener("click", copy)
 document.querySelector("#copy-2").addEventListener("click", copy)
+document.querySelector("#renew").addEventListener("click", generatePassword)
 
 generatePassword()
